72c41b603b7107592ec41e55a195cc88686405b1
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2025-11-24 14:22:14 +0100

    chore: compatibility with PHP 8.5

--- FormBundle.php
+++ FormBundle.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class FormBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new RegisterCustomBuilders());
         $container->addCompilerPass(new RegisterCustomForms());
